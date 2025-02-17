1d930e7a47f3cfaa78f009171531000199000acb
Rodrigo <rrddpp1@gmail.com>
2025-02-17 22:57:12 +0100

    mejorar responsive de la fuente de texto en pantallas de auth, y evitar tantos valores absolutos y poco responsive

--- frontend/src/presentation/screens/auth/SignUpScreen.tsx
+++ frontend/src/presentation/screens/auth/SignUpScreen.tsx
@@ -1,5 +1,11 @@
 import React, {useState} from 'react';
-import {View, Pressable} from 'react-native';
+import {
+  View,
+  Pressable,
+  ScrollView,
+  SafeAreaView,
+  Dimensions,
+} from 'react-native';
 import {globalStyles} from '../../../config/theme/theme.ts';
 import {NavigationProp, useNavigation} from '@react-navigation/native';
 import {RootStackParams} from '../../navigator/Navigator.tsx';
@@ -11,6 +17,8 @@ import {useFormValidation} from '../../hooks/useFormValidation.tsx';
 import {useFocus} from '../../hooks/useFocus.tsx';
 import {Text} from 'react-native-paper';
 import {CustomButton} from '../../components/common/CustomButton.tsx';
+
+const {width, height} = Dimensions.get('window');
 
 export const SignUpScreen = () => {
   const {fields, error, handleInputChange, validateForm} = useFormValidation({
@@ -24,6 +32,7 @@ export const SignUpScreen = () => {
   >();
 
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
   const handleSignUp = () => {
     if (validateForm()) {
       navigation.navigate('SignupSuccess');
@@ -31,95 +40,116 @@ export const SignUpScreen = () => {
   };
 
   return (
-    <View style={globalStyles.container}>
-      <Text
-        variant="headlineLarge"
-        style={{marginBottom: 40, fontWeight: '800'}}>
-        Crear cuenta
-      </Text>
-      <Input
-        label="Nombre"
-        placeholder="John Doe"
-        value={fields.name}
-        onChangeText={value => handleInputChange('name', value)}
-        isFocused={focusedInput === 'name'}
-        onFocus={() => handleFocus('name')}
-        onBlur={handleBlur}
-      />
-      <Input
-        label="Correo electrónico"
-        placeholder="hello@example.com"
-        value={fields.email}
-        onChangeText={value => handleInputChange('email', value)}
-        isFocused={focusedInput === 'email'}
-        onFocus={() => handleFocus('email')}
-        onBlur={handleBlur}
-        keyboardType="email-address"
-      />
-      <Input
-        label="Contraseña"
-        placeholder="Password"
-        value={fields.password}
-        onChangeText={value => handleInputChange('password', value)}
-        isFocused={focusedInput === 'password'}
-        onFocus={() => handleFocus('password')}
-        onBlur={handleBlur}
-        isPassword
-        showPassword={showPassword}
-        togglePasswordVisibility={() => setShowPassword(!showPassword)}
-      />
+    <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+      <ScrollView
+        contentContainerStyle={{
+          flex: 1,
+          padding: 20,
+          backgroundColor: '#ffffff',
+          marginTop: height * 0.02,
+        }}
+        showsVerticalScrollIndicator={false}>
+        <Text
+          variant="headlineLarge"
+          style={{marginBottom: 40, fontWeight: '800'}}>
+          Crear cuenta
+        </Text>
+        <Input
+          label="Nombre"
+          placeholder="John Doe"
+          value={fields.name}
+          onChangeText={value => handleInputChange('name', value)}
+          isFocused={focusedInput === 'name'}
+          onFocus={() => handleFocus('name')}
+          onBlur={handleBlur}
+        />
+        <Input
+          label="Correo electrónico"
+          placeholder="hello@example.com"
+          value={fields.email}
+          onChangeText={value => handleInputChange('email', value)}
+          isFocused={focusedInput === 'email'}
+          onFocus={() => handleFocus('email')}
+          onBlur={handleBlur}
+          keyboardType="email-address"
+        />
+        <Input
+          label="Contraseña"
+          placeholder="Password"
+          value={fields.password}
+          onChangeText={value => handleInputChange('password', value)}
+          isFocused={focusedInput === 'password'}
+          onFocus={() => handleFocus('password')}
+          onBlur={handleBlur}
+          isPassword
+          showPassword={showPassword}
+          togglePasswordVisibility={() => setShowPassword(!showPassword)}
+        />
 
-      <Text
-        variant="bodyLarge"
-        style={{
-          color: '#4c5667',
-          marginTop: 5,
-          marginBottom: 20,
-          textAlign: 'center',
-        }}>
-        Al continuar, aceptas nuestros{' '}
         <Text
           variant="bodyLarge"
-          style={{color: '#FF5A5F', textAlign: 'center', fontWeight: 'bold'}}>
-          términos y condiciones
-        </Text>
-      </Text>
-
-      <CustomButton
-        text="Registrarse"
-        textStyle={globalStyles.buttonText}
-        style={{marginBottom: 30}}
-        onPress={handleSignUp}
-      />
-
-      <SeparatorWithText text="o" />
-
-      <Pressable
-        style={[globalStyles.googleButton, {marginTop: 30}]}
-        onPress={() => console.log('Pressed')}>
-        <View style={globalStyles.googleButtonContent}>
-          <GenericIcon name="logo-google" style={{marginRight: 15}} />
-          <Text variant="bodyLarge" style={{color: '#444'}}>
-            Continuar con Google
-          </Text>
-        </View>
-      </Pressable>
-
-      <View style={globalStyles.footer}>
-        <Text variant="bodyLarge" style={{color: '#818181'}}>
-          ¿Ya tienes una cuenta?{' '}
-        </Text>
-        <Pressable onPress={() => navigation.navigate('Signin')}>
+          style={{
+            color: '#4c5667',
+            marginTop: 5,
+            marginBottom: 20,
+            textAlign: 'center',
+          }}>
+          Al continuar, aceptas nuestros{' '}
           <Text
             variant="bodyLarge"
             style={{color: '#FF5A5F', textAlign: 'center', fontWeight: 'bold'}}>
-            Inicia sesión aquí
+            términos y condiciones
           </Text>
-        </Pressable>
-      </View>
-      <View />
+        </Text>
 
-      {error && <Message error={error} />}
-    </View>
+        <CustomButton
+          text="Registrarse"
+          textStyle={globalStyles.buttonText}
+          style={{marginBottom: 30}}
+          onPress={handleSignUp}
+        />
+
+        <SeparatorWithText text="o" />
+
+        <Pressable
+          style={[globalStyles.googleButton, {marginTop: 30}]}
+          onPress={() => console.log('Pressed')}>
+          <View style={globalStyles.googleButtonContent}>
+            <GenericIcon name="logo-google" style={{marginRight: 15}} />
+            <Text variant="bodyLarge" style={{color: '#444'}}>
+              Continuar con Google
+            </Text>
+          </View>
+        </Pressable>
+
+        {/* Spaced footer that should always be visible */}
+        <View
+          style={[
+            globalStyles.footer,
+            {
+              marginTop: 'auto', // Push to bottom of available space
+              marginBottom: 20,
+              paddingBottom: 10,
+            },
+          ]}>
+          <Text variant="bodyLarge" style={{color: '#818181'}}>
+            ¿Ya tienes una cuenta?{' '}
+          </Text>
+          <Pressable onPress={() => navigation.navigate('Signin')}>
+            <Text
+              variant="bodyLarge"
+              style={{
+                color: '#FF5A5F',
+                textAlign: 'center',
+                fontWeight: 'bold',
+              }}>
+              Inicia sesión aquí
+            </Text>
+          </Pressable>
+        </View>
+
+        {error && <Message error={error} />}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
