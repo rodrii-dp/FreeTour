a11b84c4867aefaf98141c79728ea3bfadfb6f8b
OscarZ <oscar.haotian.ceb@immune.institute>
2025-05-29 23:42:55 +0200

    Hola

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(3000);
+
+  console.log(app.getHttpServer()._events.request._router.stack
+    .filter(r => r.route)
+    .map(r => r.route.path));
 }
 bootstrap();
