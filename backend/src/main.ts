f8fa25cde90d2f7cc246e49c8e7a9e19258f0fd6
Rodrigo <rrddpp1@gmail.com>
2025-06-04 11:55:24 +0200

    arreglar listen

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
-
   app.enableCors({
     origin: 'https://desperate-joan-freetour-ab6f618a.koyeb.app',
     credentials: true, // si usas cookies
